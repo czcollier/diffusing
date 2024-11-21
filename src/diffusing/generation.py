c2a7ef0c757a75a39cd6e6814ddc2490ea904839
Chris Collier <czcollier@google.com>
2024-11-21 19:49:03 +0000

    add more project belives se
    Change-Id: Ic5f021a9be52e09b2b7dd69722cafc9f5f411715

--- src/diffusing/generation.py
+++ src/diffusing/generation.py
@@ -17,42 +17,11 @@ import piexif
 import imagehash
 
 from sd_embed.embedding_funcs import get_weighted_text_embeddings_sd3, get_weighted_text_embeddings_flux1
-
-@dataclass
-class Subject:
-  person: str
-  pose: str = ""
-  scene: str = ""
-  clothing: str = ""
-  photo_quality: str = ""
-  neg_prompt: str = None
-
-  def build_prompt(self):
-    descr = ", ".join([self.person, self.pose, self.scene, self.clothing])
-    prompt = ", ".join([descr, self.photo_quality])
-    return prompt
-
-@dataclass
-class Adapter:
-  path: str
-  weights_file: str
-  name: str
-  scale: float = 1.0
-
-@dataclass
-class GenerationConfig:
-  guidance_scale: float = 7.5
-  steps: int = 30
-  small_dim: int = 896
-  large_dim: int = 1152
-  landscape: bool = False
-  num_samples: int = 1
-
-  def height(self):
-    return self.small_dim if self.landscape else self.large_dim
-
-  def width(self):
-    return self.large_dim if self.landscape else self.small_dim
+from generation_model import (
+    GenerationConfig,
+    Subject,
+    Adapter
+)
 
 @dataclass
 class PicGenerator(abc.ABC):
